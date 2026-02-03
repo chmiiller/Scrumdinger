8ac4a61ef5e301e844694cf013bd503812b19d60
Carlos Zinato <carlosh.lz@hotmail.com>
2026-02-03 22:16:13 +0100

    project kickoff

---
+++ scrumdinger/scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Carlos Zinato on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
