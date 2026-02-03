b106a38aa95f517c66f04e6a432487f86d269975
Carlos Zinato <carlosh.lz@hotmail.com>
2026-02-03 22:58:19 +0100

    added DailyScrum model

---
+++ scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  scrumdinger
+//
+//  Created by Carlos Zinato on 03/02/2026.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
