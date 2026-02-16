163bb28518a5d473b1583e73d3b669e80745c575
Carlos Zinato <carlosh.lz@hotmail.com>
2026-02-16 19:57:11 +0100

    adding meeting history

--- scrumdinger/Views/MeetingView.swift
+++ scrumdinger/Views/MeetingView.swift
@@ -33,18 +33,28 @@ struct MeetingView: View {
         .padding()
         .foregroundStyle(Color(scrum.theme.accentColor))
         .onAppear {
-            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes,
-                             attendeeNames: scrum.attendees.map({ $0.name }))
-            scrumTimer.speakerChangedAction = {
-                player.seek(to: .zero)
-                player.play()
-            }
-            scrumTimer.startScrum()
+            startScrum()
         }
         .onDisappear {
-            scrumTimer.stopScrum()
+            endScrum()
         }
         .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    private func startScrum() {
+        scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes,
+                         attendeeNames: scrum.attendees.map({ $0.name }))
+        scrumTimer.speakerChangedAction = {
+            player.seek(to: .zero)
+            player.play()
+        }
+        scrumTimer.startScrum()
+    }
+    
+    private func endScrum() {
+        scrumTimer.stopScrum()
+        let history = History(attendees: scrum.attendees)
+        scrum.history.insert(history, at: 0)
     }
 }
 
