04fa8cce8a3da886a0c818885b6519d5f38d6a7b
Carlos Zinato <carlosh.lz@hotmail.com>
2026-02-18 18:12:14 +0100

    add the circle

---
+++ scrumdinger/Views/MeetingTimerView.swift
@@ -0,0 +1,48 @@
+//
+//  MeetingTimerView.swift
+//  scrumdinger
+//
+//  Created by Carlos Zinato on 18/02/2026.
+//
+
+import SwiftUI
+import ThemeKit
+import TimerKit
+
+struct MeetingTimerView: View {
+    let speakers: [ScrumTimer.Speaker]
+    let theme: Theme
+    
+    private var currentSpeaker: String {
+        speakers.first { !$0.isCompleted }?.name ?? "Someone"
+    }
+
+    var body: some View {
+        Circle()
+            .strokeBorder(lineWidth: 24)
+            .overlay {
+                VStack {
+                    Text(currentSpeaker)
+                        .font(.title)
+                    Text("is speaking")
+                }
+                .accessibilityElement(children: .combine)
+                .foregroundStyle(theme.accentColor)
+            }
+            .overlay {
+                ForEach(speakers) { speaker in
+                    if speaker.isCompleted, let index = speakers.firstIndex(where: { $0.id == speaker.id }) {
+                        SpeakerArc(speakerIndex: index, totalSpeakers: speakers.count)
+                            .rotation(Angle(degrees: -90))
+                            .stroke(theme.mainColor, style: StrokeStyle(lineWidth: 14))
+                    }
+                }
+            }
+            .padding(.horizontal)
+    }
+}
+
+#Preview {
+    let speakers = [ScrumTimer.Speaker(name: "Cake", isCompleted: true), ScrumTimer.Speaker(name: "Lola", isCompleted: false)]
+    MeetingTimerView(speakers: speakers, theme: .yellow)
+}
